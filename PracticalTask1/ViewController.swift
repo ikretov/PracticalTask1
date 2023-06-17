bca174549d4592afc904d38adfd6769be9d64dce
i_kretov <kretov.ig@mail.ru>
2023-06-17 15:15:49 +0900

    Initial Commit

---
+++ PracticalTask1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticalTask1
+//
+//  Created by Igor Kretov on 17.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
